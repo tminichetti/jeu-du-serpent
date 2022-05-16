a2a6dcd37c67845118c1c9ba47d9ee6cfa3c09e2
Nailyk2509 <kylian.hiezely@outlook.fr>
2022-05-16 10:35:14 +0200

    Fonction bonus

---
+++ bonus.php
@@ -0,0 +1,20 @@
+<?php
+Class Bonus() {
+    public function bonus(int $case) {
+        if ($case = 20)
+        {
+            $case = 35;
+        };
+        if ($case = 2)
+        {
+            $case = 17;
+        };
+        if ($case = 31)
+        {
+            $case = 43;
+        };
+        return $case;
+        
+    }
+}
+?>
