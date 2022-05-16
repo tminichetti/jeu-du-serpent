4e150409dcb225332416a9dd7611973f80f00a4a
Nailyk2509 <kylian.hiezely@outlook.fr>
2022-05-16 10:41:57 +0200

    Fonction more50
    Si le joueur dépasse 50

---
+++ more50.php
@@ -0,0 +1,12 @@
+<?php
+Class More50() {
+    public function more50(int $case) {
+        if ($case >= 50)
+        {
+            $case = 25;
+        };
+        return $case;
+        
+    }
+}
+?>
