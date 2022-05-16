ce27c160bd8dba5b42033b7005e5a71b739c57d8
Nailyk2509 <kylian.hiezely@outlook.fr>
2022-05-16 10:41:22 +0200

    Correction syntaxes

--- bonus.php
+++ bonus.php
@@ -1,15 +1,15 @@
 <?php
 Class Bonus() {
     public function bonus(int $case) {
-        if ($case = 20)
+        if ($case == 20)
         {
             $case = 35;
         };
-        if ($case = 2)
+        if ($case == 2)
         {
             $case = 17;
         };
-        if ($case = 31)
+        if ($case == 31)
         {
             $case = 43;
         };
--- trap.php
+++ trap.php
@@ -1,15 +1,15 @@
 <?php
 Class Bonus() {
     public function bonus(int $case) {
-        if ($case = 37)
+        if ($case == 37)
         {
             $case = 12;
         };
-        if ($case = 14)
+        if ($case == 14)
         {
             $case = 7;
         };
-        if ($case = 46)
+        if ($case == 46)
         {
             $case = 33;
         };
