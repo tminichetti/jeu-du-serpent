dd09f222de99a711e72eed9488d9c465a634d080
Nailyk2509 <kylian.hiezely@outlook.fr>
2022-05-16 10:35:32 +0200

    Fonction trap

---
+++ trap.php
@@ -0,0 +1,20 @@
+<?php
+Class Bonus() {
+    public function bonus(int $case) {
+        if ($case = 37)
+        {
+            $case = 12;
+        };
+        if ($case = 14)
+        {
+            $case = 7;
+        };
+        if ($case = 46)
+        {
+            $case = 33;
+        };
+        return $case;
+        
+    }
+}
+?>
